24c19c0faeb74a207a7ac50b988c01d60a3d99da
stooshiev <102243012+stooshiev@users.noreply.github.com>
2023-09-16 14:26:09 -0400

    Update mailmaker.py

--- mailmaker.py
+++ mailmaker.py
@@ -1,18 +1,20 @@
 # mailmaker.py is a program that sends an email
-# running from the command line:
+
+# example of running from the command line:
 #     mailmaker.py sender.txt password.txt receiver.txt subject.txt body.txt
-# running from another python file:
+
+# example of running from another python file:
 #     import mailmaker
-#     # sender, password, receiver, subject, and body are strings
+#     mailmaker.sendEmail('sender.txt', 'password.txt', 'receiver.txt', 'subject.txt', 'body.txt')
+
+#     arguments
 #     mailmaker.sendEmail(sender, password, receiver, subject, body)
-
-#         sender.txt - a single gmail address, the sender
-#         password.txt - app password for the sender's gmail
-#         receiver.txt - a single email address to send the email to
-#         subject.txt - subject of the email
-#         body.txt - main text of the email
+#         sender - a single gmail address, the sender
+#         password - app password for the sender's gmail
+#         receiver - a single email address to send the email to
+#         subject - subject of the email
+#         body - main text of the email
 #         NOTE: I reccommend that all text files contain only characters that can be typed on a US keyboard
-
 
 
 
@@ -20,14 +22,14 @@
 import sys;
 args = sys.argv[1 :]
 
-# sender.txt password.txt receiver.txt subject.txt body.txt
+# sender password receiver subject body
 
 from email.message import EmailMessage
 import ssl
 import smtplib
 
-
 def sendEmail(sender, password, receiver, subject, body):
+    sender, password, receiver, subject, body = [open(arg).read() for arg in (sender, password, receiver, subject, body)]
 
     context = ssl.create_default_context()
     em = EmailMessage()
@@ -38,10 +40,11 @@ def sendEmail(sender, password, receiver, subject, body):
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(sender, password)
+        print(em.as_string())
         smtp.sendmail(sender, receiver, em.as_string())
         print('sent')
 
 if __name__ == '__main__':
     if len(args) != 5:
         args = 'sender.txt password.txt receiver.txt subject.txt body.txt'.split()
-    sendEmail(*[open(arg).read() for arg in args])
+    sendEmail(*args)
